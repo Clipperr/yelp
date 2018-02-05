1bf9b4a8390e1605984a04a7d5ef2bb52481083d
mrpycharm <ahmed.dhanani26@gmail.com>
2018-02-05 13:59:32 +0500

    add auth.py for authentication decorators

--- yelp/users_views.py
+++ yelp/users_views.py
@@ -8,3 +8,15 @@ from yelp import app
 @app.route('/')
 def index():
     return jsonify({'message' : 'Hello World 2'})
+
+
+@app.route('/search/<alias>', methods=['GET'])
+def search_alias(alias):
+    """ searches if the alias is usable """
+    pass
+
+
+@app.route('/signup', methods=['POST'])
+def signup(data):
+    """ registers a user to the system """
+    pass
