2f92e0f1fe397df5d7b5a2452b6358190f4b9b49
mrpycharm <ahmed.dhanani26@gmail.com>
2018-02-11 17:55:39 +0500

    add mongodb client

---
+++ yelp/config/config.py
@@ -0,0 +1,6 @@
+''' Application configuration for the Polygon app '''
+
+DB_USER = 'clipperdb'
+BB_PASS = 'Polygon123'
+DB_URL = 'polygon-0xfkc.mongodb.net'
+DB_DB = 'polygondev'
--- yelp/database.py
+++ yelp/database.py
@@ -1,1 +1,12 @@
 ''' Main connection building and access to the database for Polygon application '''
+
+# --- pymongo imports
+from pymongo import MongoClient
+
+# --- module imports
+from config import DB_USER, DB_PASS, DB_URL, DB_DB
+
+# make a connection to the db
+client = MongoClient('mongodb+srv://' + DB_USER + ':' + DB_PASS + DB_URL + '/' + DB_DB)
+
+
